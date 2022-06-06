b0d0fd48984234cf36cd607a4da195fa0b2eac18
Romina Guimaraens <5071@holbertonschool.com>
2022-06-06 09:19:36 -0700

    task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
-
 /**
- * Prints string
+ * Prints string and returns null
  */
 
+/**
+ * main funcion prints string
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+/**
+ * returns null
+ */
 	return (0);
 }
