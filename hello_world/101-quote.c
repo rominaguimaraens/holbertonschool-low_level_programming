4e37a462e1f19331b80c98483c111ddf9c831d5d
Romina Guimaraens <5071@holbertonschool.com>
2022-06-06 10:19:28 -0700

    task 8

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry Point
  *
- * Retun: Return 0 (Success)
+ * Return: Return 0 (Success)
  */
 int main(void)
 {
 	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	return (0);
+	return (1);
 }
