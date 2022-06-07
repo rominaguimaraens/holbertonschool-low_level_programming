a539b899b14aca3698062bb54792939d6d0d8c3a
Romina Guimaraens <5071@holbertonschool.com>
2022-06-07 10:23:19 -0700

    $1

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Resturns 0 (Success)
+ */
+
+int main(void)
+{
+	char n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		n++;
+		if (n < '9')
+		{
+			putchar(,);
+			putchar( );
+		}
+	}
+	putchar('\n');
+	return (0);
+}
