0204091d60d12a6954bfdc6a47ea4cf845124107
Romina Guimaraens <5071@holbertonschool.com>
2022-06-07 07:01:27 -0700

    $1

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Resturns 0 (Success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	return (0);
+}
