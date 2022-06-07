2926cd489aae06692ea7f3b024259aab45b0dba1
Romina Guimaraens <5071@holbertonschool.com>
2022-06-07 07:47:25 -0700

    $1

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Resturns 0 (Success)
+ */
+
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+	return (0);
+}
