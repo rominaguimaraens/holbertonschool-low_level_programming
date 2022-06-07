f6105ef8ca7895c03a98c79ab98ff2256d54e0c1
Romina Guimaraens <5071@holbertonschool.com>
2022-06-07 06:32:14 -0700

    $1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -20,9 +20,9 @@ int main(void)
 	printf("Last digit of %d ", n);
 	if (lastdig > 5)
 		printf("is %d and is greater than 5\n", lastdig);
-	if (lastdig < 6)
+	else if (lastdig < 6)
 		printf("is %d and is less than 6 and not 0\n", lastdig);
-	if (lastdig == 0)
+	else
 		printf("is %d and is 0\n", lastdig);
 	return (0);
 }
