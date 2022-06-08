478a61b4b71b5eaf4e737530a07bbb6a940c3f18
Romina Guimaraens <5071@holbertonschool.com>
2022-06-08 07:55:54 -0700

    $1

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -9,25 +9,28 @@ int main(void)
 {
 	int n;
 	int m;
- 	m = '1';
+	n = '0';
+	m = '1';
 
-	for (n = '0'; n <= '9'; m++)
-	{	putchar(n);
+	while (n < '9')
+	{
+		putchar(n);
+		if (m = '9')
+		{
+			n++
+		}
+
 		if (m == n)
 		{
 			continue;
 		}
+
 		putchar(m);
 		putchar(',');
-		putchar(' ' );
-		if (m == '9')
-		{
-			m = '1';
-			n++;
-		}
+		putchar(' ');
+		m++;
+	}
 
-		
-	} 
 	putchar('\n');
 	return (0);
 }
