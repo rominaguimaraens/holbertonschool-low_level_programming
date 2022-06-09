f69cf7bb4348eea3d7c0c3f2aaa5c0df506efae2
Romina Guimaraens <5071@holbertonschool.com>
2022-06-09 10:30:06 -0700

    $1

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*_islower - checks i lowercase
+*
+* Return: returns 0 (success)
+*/
+int _islower(int c)
+{
+	if ((c >= 'a') || (c <= 'z'))
+		return 1;
+	else
+		return (0);
+}
