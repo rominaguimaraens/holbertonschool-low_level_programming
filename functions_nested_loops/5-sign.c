93fc7e7e8989734c6f66f2d3c2bebd85a3d45124
Romina Guimaraens <5071@holbertonschool.com>
2022-06-09 11:13:10 -0700

    $1

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -15,16 +15,16 @@ int print_sign(int n)
 		return (1);
 	}
 
-	if (n < '0')
+	if (n == 0)
 	{
-		_putchar('-');
-		return (-1);
+		_putchar('0');
+		return (0);
 	}
 
 
 	else
 	{
-		_putchar('0');
-		return (0);
+		_putchar('-');
+		return (-1);
 	}
 }
