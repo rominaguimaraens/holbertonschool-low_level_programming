20d2651c8702b3947628d958c21de10efdc1f611
Romina Guimaraens <5071@holbertonschool.com>
2022-06-10 16:34:26 -0700

    task 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_line - prints a line
+*
+* @ i: times it's printing
+*/
+void print_line(int n)
+{
+	int i = 1;
+	
+	while (i >= n)
+	{
+		_putchar('_');
+	}
+	_putchar(10);
+}
