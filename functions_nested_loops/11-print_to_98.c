8c8b43ddf8215601e4cb89e4321ec29a6c1508ca
Romina Guimaraens <5071@holbertonschool.com>
2022-06-10 09:48:39 -0700

    $1

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_to_98 - prints fomr n to 98
+*
+* @n: number to start at
+*/
+void print_to_98(int n)
+{
+	while (n <= 98)
+	{
+		n++
+		if (n == 98)
+		{
+			printf("n");
+		}
+		else
+		{
+			printf("n, ");
+		}
+
+	}
+}
