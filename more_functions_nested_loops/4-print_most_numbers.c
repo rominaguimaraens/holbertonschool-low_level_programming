e0f276b8f53e2b87a266a5aaba77a99e13b7130e
Romina Guimaraens <5071@holbertonschool.com>
2022-06-10 15:10:20 -0700

    task 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	for (n = '0', n <= '9', n++)
+	{
+		if ((n = '4' ) || (n = '4'))
+		{
+			continue;
+		}
+		_putchar(n);
+	}
+	_putchar('\n');
+}
