1090cf160b4e114df4fbf452db7d50f7410be713
Romina Guimaraens <5071@holbertonschool.com>
2022-06-09 18:19:40 -0700

    $1

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include main.h
+/**
+* _abs - absolute value of integrer
+*
+* @i: integrer
+*
+* Return: i
+*/
+
+int _abs(int i)
+{
+	if (i >= '0')
+	{
+		return (i);
+	}
+
+	else
+	{
+		i = (i * (-1));
+		return (i);
+	}
+}
