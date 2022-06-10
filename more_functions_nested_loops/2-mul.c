c75b3b35b49668c8072ec1691315113fbc562dc8
Romina Guimaraens <5071@holbertonschool.com>
2022-06-10 14:41:40 -0700

    task 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* mul - multiply up two numbers
+*
+* @a: first number
+*
+* @b: second number
+*
+* Return: result of multiply
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
