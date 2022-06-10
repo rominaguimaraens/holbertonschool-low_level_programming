a86b085142e58c5ce8ddd09391024f2df561e090
Romina Guimaraens <5071@holbertonschool.com>
2022-06-10 10:00:57 -0700

    $1

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,4 @@
-#include "main.h"
+i#include "main.h"
 #include <stdio.h>
 /**
 * print_to_98 - prints fomr n to 98
@@ -12,11 +12,11 @@ void print_to_98(int n)
 		n++;
 		if (n == 98)
 		{
-			putchar(n + 48);
+			putchar('n' + 48);
 		}
 		else
 		{
-			putchar(n + 48);
+			putchar('n' + 48);
 			putchar(',');
 			putchar(' ');
 		}
