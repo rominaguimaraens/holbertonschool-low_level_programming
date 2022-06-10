a807561314a4f86f4ec86a98a8b45dc560c7f2eb
Romina Guimaraens <5071@holbertonschool.com>
2022-06-10 09:38:27 -0700

    $1

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* add - add up two numbers
+*
+* @i: first number
+*
+* @j: second number
+*
+* Return: result of add
+*/
+int add(int i, int j)
+{
+	int i, j;
+
+	return (i + j);
+}
