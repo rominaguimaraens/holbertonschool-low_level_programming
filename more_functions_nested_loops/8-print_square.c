a0314dcf5bb420ce9dffc4d3caa4c98f344aeeaf
Romina Guimaraens <5071@holbertonschool.com>
2022-06-11 15:38:49 -0700

    task 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* print_square - prints a square
+*
+* @size: size of square
+*
+* @ i: times it's printing
+*/
+void print_square(int size)
+{
+	int i = 1;
+
+	if (size <= 0)
+	{
+		_putchar(10);
+	}
+	else
+	{
+		while (i <= size)
+		{
+			_putchar('#');
+		}
+		_putchar(10);
+	}
+}
