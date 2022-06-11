cd57fcaa8f1f3a52ba421ef722b1ef380d4f8c3d
Romina Guimaraens <5071@holbertonschool.com>
2022-06-11 15:19:10 -0700

    task 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+* print_diagonal - prints a diagonal line
+*
+* @n: number of times to print
+*/
+void print_diagonal(int n)
+{
+	int i, s;
+
+	if (n <= 0)
+	{
+		_putchar(10);
+	}
+	else
+	{
+		for (i = 1; i >= n; i++)
+		{
+	
+			_putchar(92);
+			_putchar(10);
+			if (i != n)
+			{
+				for (s = 0; s < i; s++)
+				{
+					_putchar(32);
+				}
+			}
+		}
+	}
+}
