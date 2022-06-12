4570e9a8be82244564e9277e99d29d23499ad567
Romina Guimaraens <5071@holbertonschool.com>
2022-06-12 15:32:32 -0700

    task 3

--- debugging/3-print_remaining_days.c
+++ debugging/3-print_remaining_days.c
@@ -20,11 +20,11 @@ void print_remaining_days(int month, int day, int year)
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 366 - day);
 		}
-		else if(month == 2 && day == 60)
+		else if (month == 2 && day == 60)
 		{
 			day++;
 			printf("Day of the year: %d\n", day - 1);
-			printf("Remaining days: %d\n", 366 - day +1);
+			printf("Remaining days: %d\n", 366 - day + 1);
 		}
 	}
 	else
