8544d06e55889dd07de702525e7e2ba9b86f2dea
Romina Guimaraens <5071@holbertonschool.com>
2022-06-12 15:00:12 -0700

    task 2

--- debugging/main.h
+++ debugging/main.h
@@ -1,4 +1,3 @@
-carrie@ubuntu:/debugging$ cat main.h
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -6,6 +5,7 @@ carrie@ubuntu:/debugging$ cat main.h
 
 int largest_number(int a, int b, int c);
 void positive_or_negative(int i);
-void print_remaining_days(int month, int day, int year;)
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month; int day);
 
 #endif /* MAIN_H */
