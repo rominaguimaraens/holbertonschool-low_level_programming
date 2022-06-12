adcff5a1f5755dc23a4546092e434ce6599b2f66
Romina Guimaraens <5071@holbertonschool.com>
2022-06-12 15:04:42 -0700

    task 2

--- debugging/2-largest_number.c
+++ debugging/2-largest_number.c
@@ -32,6 +32,14 @@ int largest_number(int a, int b, int c)
     {
     	largest = a;
     }
+    else if (a == c && c > b)
+    {
+    	largest = c;
+    }
+    else if (a == b && b == c)
+    {
+    	largest = b;
+    }
 
     return (largest);
 }
