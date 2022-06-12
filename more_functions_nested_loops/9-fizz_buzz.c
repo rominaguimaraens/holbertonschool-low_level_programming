cb8493c79fde25dff81d53d7448867e2d8e0bbb7
Romina Guimaraens <5071@holbertonschool.com>
2022-06-11 17:03:14 -0700

    task 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Return: returns 0 (success)
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 1; n < 101; n++)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", n);
+		}
+		if (n != 100)
+		{
+			putchar(' ');
+		}
+	}
+	return (0);
+}
