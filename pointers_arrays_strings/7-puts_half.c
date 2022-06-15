690c12b003c0f09e626d53416367e765bf5c1728
Romina Guimaraens <5071@holbertonschool.com>
2022-06-15 15:00:37 -0700

    task 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* puts_half - prints hals of string
+* @str: string
+*/
+void puts_half(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+
+		i++;
+	}
+
+	i = i - 1;
+
+	if (i % 2 != 0)
+	{
+		int n;
+
+		for (n = i / 2; str[n]; n++)
+		{
+			_putchar(str[n]);
+		}
+	}
+	_putchar(10);
+}
