593ac39cfc6b7e52dc7bce2e60d23b5fab8f0972
Romina Guimaraens <5071@holbertonschool.com>
2022-06-17 08:22:52 -0700

    task 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strncpy - copies string
+ * @dest: destination
+ * @src: string
+ * @n: number
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
