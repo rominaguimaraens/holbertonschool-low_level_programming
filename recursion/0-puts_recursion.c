b2e49fc33f259630736aad2e300e41ac43e19f98
Romina Guimaraens <5071@holbertonschool.com>
2022-06-20 07:56:24 -0700

    task 0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -5,12 +5,11 @@
  */
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	while (s[i])
+	if (*s == 0)
 	{
-		_putchar(s[i]);
-		i++;
+		_putchar(10);
+		return;
 	}
-	_putchar(10);
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
