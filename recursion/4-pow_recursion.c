dfe583a8ec608bd733ba0fa63da820f1c303faff
Romina Guimaraens <5071@holbertonschool.com>
2022-06-20 10:34:43 -0700

    task  4

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -3,12 +3,17 @@
  * _pow_recursion - returns the valueof x to the power of y
  * @x: x
  * @y: y
+ * Return: x to the power of y
  */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 	{
 		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
 	}
 	return (x * _pow_recursion(x, y - 1));
 }
