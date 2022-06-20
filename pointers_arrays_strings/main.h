3129b2a7ab4f67f9a5127d2d023c50e7c0ab391b
Romina Guimaraens <5071@holbertonschool.com>
2022-06-19 17:59:35 -0700

    task 0

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -21,5 +21,12 @@ char *leet(char *);
 int is_delimiter(char c);
 char capitalize(char c);
 char *cap_string(char *s);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
 
 #endif
