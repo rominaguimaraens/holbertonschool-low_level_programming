82b9ac69c391fa4b6b2b6103b03ae08f3118d8ff
Romina Guimaraens <5071@holbertonschool.com>
2022-06-20 12:32:29 -0700

    task  6

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "main.c"
+/**
+ * _helper - gets prime number
+ * @i: divide by
+ * @n: num to check
+ */
+int _helper(int i, int n)
+{
+	if (i == 1)
+	{
+		return (1);
+	}
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (_helper(i - 1, n));
+	}
+}
+/**
+ * is_prime_number - is it a prime number
+ * @n: number to check
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (_helper(n - 1, n));
+}
--- recursion/main.h
+++ recursion/main.h
@@ -10,5 +10,6 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int _squ(int n, int i);
+int _helper(int i, int n);
 
 #endif
