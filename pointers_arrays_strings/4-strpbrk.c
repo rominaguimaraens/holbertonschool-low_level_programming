960e9ed6bf7ab72b276bbfe7cf21a1e2caf1fe81
Romina Guimaraens <5071@holbertonschool.com>
2022-06-19 18:30:40 -0700

    task 4

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strpbrk - searches string for set of bytes
+ * @s: string
+ * @accept: bytes
+ * Return: s or 0
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0, j;
+
+	while (s[i])
+	{
+		x = 0;
+		while (accept[j])
+		{
+			if (s[i] == accept[j])
+			{
+				return (&s[i]);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
