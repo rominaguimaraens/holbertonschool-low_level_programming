c84bed0a3e2bde56c58dbc3f993eb61981fc33e5
Romina Guimaraens <5071@holbertonschool.com>
2022-06-19 18:18:00 -0700

    task 1

--- pointers_arrays_strings/1-main.c
+++ pointers_arrays_strings/1-main.c
@@ -2,19 +2,45 @@
 #include <stdio.h>
 
 /**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+        unsigned int i;
+
+        i = 0;
+        while (i < size)
+        {
+                if (i % 10)
+                {
+                        printf(" ");
+                }
+                if (!(i % 10) && i)
+                {
+                        printf("\n");
+                }
+                printf("0x%02x", buffer[i]);
+                i++;
+        }
+        printf("\n");
+}
+
+/**
  * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    int a;
-    int b;
+    char buffer[98] = {0x00};
 
-    a = 98;
-    b = 42;
-    printf("a=%d, b=%d\n", a, b);
-    swap_int(&a, &b);
-    printf("a=%d, b=%d\n", a, b);
+    simple_print_buffer(buffer, 98);
+    _memset(buffer, 0x01, 95);
+    printf("-------------------------------------------------\n");
+    simple_print_buffer(buffer, 98);    
     return (0);
 }
