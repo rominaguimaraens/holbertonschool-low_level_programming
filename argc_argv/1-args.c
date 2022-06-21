3fb7d4060cf085eee2ebdc115b2e7b61d7b3572b
Romina Guimaraens <5071@holbertonschool.com>
2022-06-20 18:18:44 -0700

    task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: argc
+ * @argv: argv
+ * Return: returns 0 (Success)
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
