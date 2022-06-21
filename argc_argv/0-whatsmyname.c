4ef4fedb95293b9545a90d4b1d7ce68370bdd500
Romina Guimaraens <5071@holbertonschool.com>
2022-06-20 18:12:12 -0700

    task 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: argc
+ * @argv: argv
+ * Return: returns 0 (Success)
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
