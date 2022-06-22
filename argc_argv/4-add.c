8f0c811c9616956e0769f1e536c4c92fbcc0a7e2
Romina Guimaraens <5071@holbertonschool.com>
2022-06-21 17:56:54 -0700

    task 4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ * @argc: argc
+ * @argv: argv
+ * Return: returns 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int i = 1;
+	int j;
+	int r;
 
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	while (argv[i])
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			j++;
+		}
+		r = atoi(argv[i]);
+		i++;
+	}
+	printf("%d\n", r);
+	return (0);
+}
