d8b42f6ad92d5ccc31a0f13c0e76a1517e0863d9
Romina Guimaraens <5071@holbertonschool.com>
2022-06-23 10:32:49 -0700

    task 0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - creates an array of chars
+ * @size: size of array
+ * @c: initalizer character
+ * Return: array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+
+	s = malloc(sizeof(char) * size);
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	s[0] = c;
+	return (s);
+	free(s);
+}
