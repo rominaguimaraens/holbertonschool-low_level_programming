0cc25740544885bdaa544621468d531b1f7da810
Romina Guimaraens <5071@holbertonschool.com>
2022-06-24 10:43:56 -0700

    task 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -15,11 +15,11 @@ char *str_concat(char *s1, char *s2)
 	j = 0;
 	l = 0;
 
-	while (s1[i])
+	while (s1 != NULL && s1[i])
 	{
 		i++;
 	}
-	while (s2[j])
+	while (s 2 != NULL && s2[j])
 	{
 		j++;
 	}
