441d75e8681b0575d614f5344933c27dd149e82f
Romina Guimaraens <5071@holbertonschool.com>
2022-06-24 12:32:12 -0700

    task 4

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_grid - frees the grid space
+ * @grid: grid
+ * @height: height
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = height; i >= 0; i--)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
