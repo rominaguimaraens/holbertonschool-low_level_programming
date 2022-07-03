49bc9064bb876f0acb84025c2fb4096edaef4cef
Romina Guimaraens <5071@holbertonschool.com>
2022-07-03 14:36:43 -0700

    task 0

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - sums all args
+ * @n: number of args
+ * Return: returns res
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	unsigned int res = 0;
+	va_list a;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	va_start(a, n);
+
+	for (i = 0; i < 0; i++)
+	{
+		res += va_arg(a, int);
+	}
+	va_end(a);
+	return (res);
+}
