27de77e8a2865a15cc94bf5839fca589b08a165a
Romina Guimaraens <5071@holbertonschool.com>
2022-07-14 16:24:55 -0700

    task 2

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * add_node - adds a new node at the beginning of the linked list
+ * @head: pointer to linked list
+ * @str: what to add
+ * Return: new node
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newNode;
+
+	newNode->str = strdup(str);
+	newNode->len = strlen(str);
+	newNode->next = *head;
+	*head = newNode;
+	return (newNode);
+}
