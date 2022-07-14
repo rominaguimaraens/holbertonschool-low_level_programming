5ecb9a9c51d1501042eba8535ae7e87633828906
Romina Guimaraens <5071@holbertonschool.com>
2022-07-14 16:52:26 -0700

    task 4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_list - frees a list
+ * @head: pointer to list
+ */
+void free_list(list_t *head)
+{
+	list_t *freedom;
+
+	freedom = head;
+
+	if (freedom->str)
+	{
+		free(freedom->str);
+	}
+	head = head->next;
+	free(freedom);
+}
