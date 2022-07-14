a3d9a83506d473521e8781d001fb0ee98e58ceea
Romina Guimaraens <5071@holbertonschool.com>
2022-07-14 16:28:33 -0700

    task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -9,6 +9,12 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *newNode;
 
+	newNode = malloc(sizeof(list_t));
+
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
 	newNode->str = strdup(str);
 	newNode->len = strlen(str);
 	newNode->next = *head;
