c4f3402dc94e3d5add2c7a11417d69d29565dab3
Romina Guimaraens <5071@holbertonschool.com>
2022-07-19 09:41:22 -0700

    task 0

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -18,8 +18,8 @@ unsigned int binary_to_uint(const char *b)
 			value += b[i] - '0';
 			i++;
 	}
-	if (b[i] != '1' && b[i] != '0')
-		return (0);
-	else
+	if (b[i - 1] == '1' || b[i - 1] == '0')
 		return (value);
+	else
+		return (0);
 }
