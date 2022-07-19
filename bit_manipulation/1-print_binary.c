aeed947f06b56f7485fb21e378a1865bae055910
Romina Guimaraens <5071@holbertonschool.com>
2022-07-19 09:57:19 -0700

    task 1

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
+#include "main.h"
 /**
  * print_binary - prints number as binary
  * @n: number to convert
  */
 void print_binary(unsigned long int n)
 {
-	unsigned i;
+	int i;
+
 	for (i = 1 << 31; i > 0; i = i / 2)
 		(n & i) ? _putchar('1') : _putchar('0');
 }
