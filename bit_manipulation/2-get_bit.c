6f05ce8957b1228b8224247ba84408db09c9edf6
Romina Guimaraens <5071@holbertonschool.com>
2022-07-19 10:42:54 -0700

    task 2

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <strings.h>
 /**
  * get_bit - returns value of bit at index
  * @n: number
@@ -9,8 +10,9 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i = 0;
+	unsigned int j = sizeof(n);
 
-	if (index >= i)
+	if (index >= i && index < j)
 		return ((n & (1 << index)) >> index);
 	else
 		return (-1);
