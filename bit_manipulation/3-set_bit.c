8932eea438569dc46e358ed4247cd5cd4e55e413
Romina Guimaraens <5071@holbertonschool.com>
2022-07-20 05:31:05 -0700

    task 3

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <strings.h>
+/**
+ * get_bit - returns value of bit at index
+ * @n: number
+ * @index: position
+ * Return: bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i = 0;
+	unsigned int j = 64;
+
+	if (index >= i && index < j)
+		return (n |= (1 << index));
+	else
+		return (-1);
+}
