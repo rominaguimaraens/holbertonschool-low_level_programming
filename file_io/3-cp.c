2fb4a64017598dadfddcf941a7d3decb709bbba7
Romina Guimaraens <5071@holbertonschool.com>
2022-07-20 18:53:47 -0700

    task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,51 +1,49 @@
 #include "main.h"
-#include <string.h>
+#include <stddef.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 /**
- * main - entry point
- * @argc: n of args
- * @argv: args
- * Return: 0 (success)
+ * main - entry point.
+ * @ac: arg count.
+ * @av: arg info.
+ * Return: 99 / 98 / 97.
  */
-
-int main(int argc, char **argv)
+int main(int ac, char *av[])
 {
-	if (argc != 3)
+	/* Check arg num */
+	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
-	copy_text(argv[1], argv[2]);
+	copy_text(av[1], av[2]);
 	exit(0);
 }
-
-/*
- * copy_text - copies text
- * @from: copy from
- * @to: copy to
+/**
+ * copy_text - copies text.
+ * @from: this file.
+ * @to: this file.
  */
-
 void copy_text(const char *from, const char *to)
 {
-	int file_from, file_to, r = 1024, w;
-	char *count[1024];
-	mode_t modsie = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+	int file_from, file_to, readMe = 5555, writeMe;
+	char *cCount[5555];
+	mode_t modsie = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
 	file_from = open(from, O_RDONLY);
-
+	/* Check if file_from exists */
 	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from);
-	exit(98);
-	}
-	file_to = open(to, O_CREAT | O_TRUNC | O_WRONLY | modsie);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from), exit(98);
+
+	file_to = open(to, O_CREAT | O_TRUNC | O_WRONLY, modsie);
 	if (file_to == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", to), exit(99);
-	r = read(file_from, count, 1024);
-	if (r == -1)
+	readMe = read(file_from, cCount, 5555);
+	if (readMe == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from), exit(98);
-	w = write(file_to, count, r);
-	if (w == -1)
+	writeMe = write(file_to, cCount, readMe);
+	if (writeMe == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", to), exit(99);
 	if (close(file_to) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
