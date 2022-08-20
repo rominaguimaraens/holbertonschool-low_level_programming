eb59dfd6e289ad99adbd09d5851bca671f4d4e58
Romina Guimaraens <5071@holbertonschool.com>
2022-08-19 17:48:43 -0700

    task 0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -3,9 +3,9 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	long unsigned int i;
-	hash_table_t *table = malloc (sizeof(hash_table_t) * size);
-	table->size = size;
-	table->array = 0;
+	hash_table_t *table = malloc (sizeof(hash_table_t));
+	table->size = size + sizeof((hash_node_t) * size);
+	table->array = malloc(sizeof(hash_node_t));
 	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
 	return (table);
