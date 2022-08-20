8fd4c01266f985f3a316c4e2a2325df1c5218f8b
Romina Guimaraens <5071@holbertonschool.com>
2022-08-19 18:44:14 -0700

    task 0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,8 +1,13 @@
 #include "hash_tables.h"
-
+/**
+ * hash_table_create - creates a new hash table
+ * @size: size of the array
+ * Return: pointer to the new hash table
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = malloc(sizeof(hash_table_t));
+
 	if (!table)
 		return (NULL);
 	table->size = size;
